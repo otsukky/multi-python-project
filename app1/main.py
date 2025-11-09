2c0d30b27b2e37c0e27937b6c0cbe224807c8333
月岡陽人 <87177693+otsukky@users.noreply.github.com>
2025-11-09 15:35:49 +0900

    環境別実行設定追加

--- app1/main.py
+++ app1/main.py
@@ -1,5 +1,11 @@
+from importlib import metadata
+
+
 def main():
     print("Hello from app1!")
+    packages = metadata.distributions()
+    for package in packages:
+        print(f"- {package.metadata['Name']} ({package.version})")
 
 
 if __name__ == "__main__":
